501321d3e9ef0b51580bbb216a6b1d05f30185a8
Rokas <r.stulga14@gmail.com>
2024-05-31 01:05:52 +0300

    yo

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const port = process.env.PORT;
 const app = express();
-app.use(cors());
+app.use(cors()); 
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
